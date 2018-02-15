71f06762f4f127a8138874d38589ce6f257b6f41
Oscar Sanchez <oscar.sanchez@orbis.com.pe>
2018-02-15 17:33:53 -0500

    feat(Refactor) refactor struct for singleResponsabilityPrinciple

---
+++ singleResponsabilityPrinciple/incorrect/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+
+class Book(object):
+    def getTitle(self):
+        return 'Im a title'
+    
+    def getAuthor(self):
+        return 'Im a author'
+
+    def printTextCurrentPage(self):
+        print 'Current page'
+
+    def printHtmlCurrentPage(self):
+        print '<div> Current page </div>'
+
+
+if __name__ == '__main__':
+    book = Book()
+    book.printTextCurrentPage()
+    book.printHtmlCurrentPage()
